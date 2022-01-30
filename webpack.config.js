8b514cddaa8050265645b96f8a41c57a28341932
Szymon Miśtal <mistalzaq@gmail.com>
2022-01-30 14:24:24 +0100

    Configure `webpack` for better bundle minifaction

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path")
+
+module.exports = {
+    entry: "./index.ts",
+    output: {
+        path: path.resolve(__dirname),
+        filename: "index.js",
+        library: {
+            type: "umd"
+        },
+        globalObject: "this"
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, use: "ts-loader" }
+        ]
+    },
+    externals: {
+        express: "commonjs express"
+    }
+}
